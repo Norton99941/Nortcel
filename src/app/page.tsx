e2680ee074ba05e1a7d59b7759199c1c7493fd17
Norton99941 <mayernort902@gmail.com>
2025-05-19 19:22:16 +0200

    Eliminar importación no utilizada de CardDescription

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Search, Users, BarChart2, Mail } from "lucide-react";
 
 export default function Home() {
